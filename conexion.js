64d6919a65a536b16b5d0c46cc29a986b75933a8
facundo.osores <facundo.osores@healthintegrator.eu>
2021-06-03 13:07:24 +0200

    add yml file with the endpoints list

--- conexion.js
+++ conexion.js
@@ -4,7 +4,7 @@ const conexion = mysql.createConnection({
   host: "localhost",
   port: "3306",
   user: "root",
-  password: "020404Ma@",
+  password: "your_password",
   database: "delilah_resto",
 });
 
